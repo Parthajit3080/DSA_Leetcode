849071cc890e5cd0d22ba0f51fcdd89a3521343d
Parthajit3080 <121250517+Parthajit3080@users.noreply.github.com>
2026-08-05 00:55:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (36.33%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,14 @@
+int searchInsert(int* nums, int numsSize, int target) {
+     int l=0, h=numsSize-1;
+    while(l<=h){
+        int mid=(l+h)/2;
+        if(nums[mid]==target)
+            return mid;
+        else if(nums[mid]<=target)
+            l=mid+1;
+        else if(nums[mid]>=target)
+            h=mid-1;
+    }
+    
+    return l;
+}
